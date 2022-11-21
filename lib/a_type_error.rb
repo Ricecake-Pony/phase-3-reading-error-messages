caf5cd79e83a343efcc4c3b010eb1287cfd1c80a
Trey <trey.delcamp@gmail.com>
2022-11-21 09:50:44 -0600

    commit tree c8f58675023a6e4a437233b39c1d21e7be301816

--- lib/a_type_error.rb
+++ lib/a_type_error.rb
@@ -1,3 +1,10 @@
-1 + 1
+# 1 + 1
 
-1 + "is the loneliest number"
+# "1" + "is the loneliest number"
+
+first_arr = [18, 22, 33, nil, 7, 6]
+puts first_arr.reverse
+
+arr1 = ["a", "b", "c", "d"]
+
+puts arr1.reverse
